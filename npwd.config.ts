e9e8acebe647d2def227184ab2818932e5fd0efe
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-02-12 22:34:27 +0100

    fix: new app id / path (#5)

--- npwd.config.ts
+++ npwd.config.ts
@@ -1,9 +1,9 @@
 import App from './src/App';
 import { GarageIcon, NotificationIcon } from './icon';
 
-export const path = '/garage';
+export const path = '/npwd_qbx_garages';
 export default () => ({
-  id: 'garage',
+  id: 'npwd_qbx_garages',
   nameLocale: 'Garage',
   color: '#fff',
   backgroundColor: '#333',
